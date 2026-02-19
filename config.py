8fb5da035ed3fad2415c56f715f641d03e7f36af
Jison Joseph Sebastian <jisonjosephsebastian7007@gmail.com>
2026-02-19 23:58:45 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Flask app configuration
+DB_HOST = 'mysql-2367c49a-radio-65e8.d.aivencloud.com'
+DB_USER = 'avnadmin'
+DB_PASSWORD = 'AVNS_H78iyks9IERyxay-J86'
+DB_NAME = 'defaultdb'
+DB_PORT = 14239
